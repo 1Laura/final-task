ec06c7e1aa6c6f400dd01a2dc47cbd3cf29eb979
Laura <baltrukone@gmail.com>
2025-03-24 14:03:27 +0200

    register and login validators

---
+++ backend/middleware/validators.js
@@ -0,0 +1,42 @@
+module.exports = {
+    validateRegister: async (req, res, next) => {
+        let {username, password} = req.body;
+
+        if (!username || !password) {
+            return res.status(400).json({message: "Username and password are required", error: true});
+        }
+
+        // Trim nereikalingus tarpus
+        username = username.trim();
+        password = password.trim();
+
+        if (username.length < 4 || username.length > 20) {
+            return res.status(400).json({message: "Username must be between 4 and 20 characters", error: true});
+        }
+
+        if (password.length < 4 || password.length > 20) {
+            return res.status(400).json({message: "Password must be between 4 and 20 characters", error: true});
+        }
+        next();
+    },
+
+    validateLogin: (req, res, next) => {
+        let {username, password} = req.body;
+
+        if (!username || !password) {
+            return res.status(400).json({message: "Username and password are required", error: true, success: false});
+        }
+
+        username = username.trim();
+        password = password.trim();
+
+        if (username.length < 4 || username.length > 20) {
+            return res.status(400).json({message: "Username must be between 4 and 20 characters", error: true});
+        }
+
+        if (password.length < 4 || password.length > 20) {
+            return res.status(400).json({message: "Password must be between 4 and 20 characters", error: true, success: false});
+        }
+        next();
+    },
+};
