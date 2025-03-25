cc97109067c036bf9065428148993e73adb61d60
Laura <baltrukone@gmail.com>
2025-03-25 09:46:30 +0200

    user schema updated

--- backend/schemas/userSchema.js
+++ backend/schemas/userSchema.js
@@ -2,22 +2,30 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: {
-        type: String,
-        required: [true, "Username is required"],
-        unique: true,
-        trim: true,
-        minlength: 4,
-        maxlength: 20,
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            minlength: 4,
+            maxlength: 20,
+        },
+        password: {
+            type: String,
+            required: true,
+            trim: true,
+            minlength: 4,
+            maxlength: 200,
+        },
+        imageUrl: {
+            type: String,
+            default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlXTd6FF5TBJbnQMU3SrwA3JAUMd3Ovu1lvw&s",
+        },
+        favorites: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Post'
+        }],
     },
-    password: {
-        type: String,
-        required: [true, "Password is required"],
-        trim: true,
-        minlength: 4,
-        maxlength: 200,
-    },
-});
+    {timestamps: true});
 
-const user = mongoose.model("users", userSchema);
-module.exports = user;
+module.exports = mongoose.model("users", userSchema);
