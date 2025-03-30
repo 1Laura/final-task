7bca57bad8f3c717c592f42d248a2647f83e376b
Laura <baltrukone@gmail.com>
2025-03-30 11:10:31 +0300

    updated zustand

--- frontend/src/store/useStore.js
+++ frontend/src/store/useStore.js
@@ -1,22 +1,32 @@
 import {create} from "zustand";
 
+
+const toggleFavorite = (favorites, postId) => {
+    const isFavorite = favorites.includes(postId);
+    return isFavorite
+        ? favorites.filter(id => id !== postId)
+        : [...favorites, postId];
+};
+
 const mainStore = create((set) => ({
     currentUser: null,
     favorites: [],
+    userInfo: null,
 
 
-    setCurrentUser: (newUser) => set({currentUser: newUser, favorites: newUser.favorites || []}),
+    setCurrentUser: (newUser) => set({
+        currentUser: newUser,
+        favorites: newUser.favorites || []
+    }),
 
-    logout: () => set({currentUser: null, favorites: []}),
-    toggleFavoriteLocally: (postId) =>
-        set((state) => {
-            const isFav = state.favorites.includes(postId);
-            return {
-                favorites: isFav
-                    ? state.favorites.filter(id => id !== postId)
-                    : [...state.favorites, postId]
-            };
-        })
+    setUserInfo: (newUserInfo) => set({userInfo: newUserInfo}),
+
+    logout: () => set({currentUser: null}),
+
+
+    toggleFavoriteLocally: (postId) => set((state) => ({
+        favorites: toggleFavorite(state.favorites, postId)
+    }))
 }));
 
 export default mainStore;
