78179a7d101704b754bdd73feaefb077ef9eb042
Laura <baltrukone@gmail.com>
2025-03-30 11:04:25 +0300

    profile and user pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import NavBar from "./components/NavBar.jsx";
 import AllPostsPage from "./pages/AllPostsPage.jsx";
 import mainStore from "./store/useStore.js";
 import CreatePostPage from "./pages/CreatePostPage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
+import UserPage from "./pages/UserPage.jsx";
 
 function App() {
     const {currentUser} = mainStore(state => state);
@@ -19,6 +21,9 @@ function App() {
                     {currentUser && <Route path="/createpost" element={<CreatePostPage/>}></Route>}
 
                     {currentUser && <Route path="/allposts" element={<AllPostsPage/>}></Route>}
+
+                    {currentUser && <Route path="/profile" element={<ProfilePage/>}></Route>}
+                    {currentUser && <Route path="/user/:username" element={<UserPage/>}></Route>}
 
 
                 </Routes>
