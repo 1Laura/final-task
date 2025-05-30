ef8303e304f63e78e22f7d45bdd07008507778dc
Laura <baltrukone@gmail.com>
2025-05-30 10:11:17 +0300

    created axios and environment files

---
+++ frontend/src/config/axios.js
@@ -0,0 +1,6 @@
+import { PROD } from "@/consts/environment";
+import axios, { AxiosError, AxiosRequestConfig } from "axios";
+
+const baseURL = PROD
+    ? "https://final-crud-app-54b7f6418183.herokuapp.com/"
+    : "http://localhost:3000/";
