04bbe9107c4cae1ef6436e3a2a879149acd85beb
Laura <baltrukone@gmail.com>
2025-05-30 10:11:36 +0300

    Updated apps.js

--- backend/App.js
+++ backend/App.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-app.use(express.json());
 require('dotenv').config();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const mainRouter = require("./router/routes");
 
-// Serve frontend static files
-const publicPath = path.join(__dirname, "public");
-app.use(express.static(publicPath));
+app.use(express.json({limit:"50mb"}));
+app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+// API routes
+app.use("/", mainRouter);
 
 // Fallback to index.html for SPA
 app.get("*", (req, res) => {
-    res.sendFile(path.join(publicPath, "index.html"));
+    res.sendFile(path.join(__dirname,"../frontend/dist", "index.html"));
 });
 
 mongoose.connect(process.env.MONGO_KEY)
@@ -24,13 +25,12 @@ mongoose.connect(process.env.MONGO_KEY)
         console.error(`❌ Failed to connect to the database:`, err);
     });
 
-
 app.use(cors());
 
 app.use("/", mainRouter);
 
 ////// Start server
-const PORT = process.env.PORT || 2002;
+const PORT = process.env.PORT || 2005;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
