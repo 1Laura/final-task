2a1cdaad3048ee430f2649abd90ff467cb9d08a7
Laura <baltrukone@gmail.com>
2025-05-30 10:12:49 +0300

    fixed port

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -3,4 +3,4 @@ import axios, { AxiosError, AxiosRequestConfig } from "axios";
 
 const baseURL = PROD
     ? "https://final-crud-app-54b7f6418183.herokuapp.com/"
-    : "http://localhost:3000/";
+    : "http://localhost:2002/";
