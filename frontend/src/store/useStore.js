1705e03773acaf7a643fc1358020b18e8974a281
Laura <baltrukone@gmail.com>
2025-09-29 13:04:38 +0300

    Updated zustand

--- frontend/src/store/useStore.js
+++ frontend/src/store/useStore.js
@@ -9,25 +9,32 @@ const toggleFavorite = (favorites, postId) => {
 };
 
 const mainStore = create((set) => ({
-    currentUser: null,
-    favorites: [],
-    userInfo: null,
-    setFavorites: (favoritesArray) => set({favorites: favoritesArray}),
+        currentUser: JSON.parse(localStorage.getItem("user")) || null,
+        favorites: [],
+        userInfo: null,
+        setFavorites: (favoritesArray) => set({favorites: favoritesArray}),
 
 
-    setCurrentUser: (newUser) => set({
-        currentUser: newUser,
-        favorites: newUser.favorites || []
-    }),
+        setCurrentUser: (newUser) => {
+            localStorage.setItem("user", JSON.stringify(newUser));
+            set({
+                currentUser: newUser,
+                favorites: newUser.favorites || []
+            });
+        },
 
-    setUserInfo: (newUserInfo) => set({userInfo: newUserInfo}),
+        setUserInfo: (newUserInfo) => set({userInfo: newUserInfo}),
 
-    logout: () => set({currentUser: null, favorites: []}),
+        logout: () => {
+            localStorage.removeItem("user");
+            localStorage.removeItem("token");
+            set({currentUser: null, favorites: []});
+        },
 
-
-    toggleFavoriteLocally: (postId) => set((state) => ({
-        favorites: toggleFavorite(state.favorites, postId)
+        toggleFavoriteLocally: (postId) => set((state) => ({
+            favorites: toggleFavorite(state.favorites, postId)
+        }))
     }))
-}));
+;
 
 export default mainStore;
