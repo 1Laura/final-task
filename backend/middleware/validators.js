9d3c2444926db46f851620adf8ef249bff8936a6
Laura <baltrukone@gmail.com>
2025-09-29 11:32:28 +0300

    Added success messages

--- backend/middleware/validators.js
+++ backend/middleware/validators.js
@@ -2,15 +2,15 @@ module.exports = {
     validateRegister: async (req, res, next) => {
         let {username, password} = req.body;
         if (!username || !password) {
-            return res.status(400).json({message: "Username and password are required", error: true});
+            return res.status(400).json({message: "Username and password are required", error: true, success: false});
         }
         username = username.trim();
         password = password.trim();
         if (username.length < 4 || username.length > 20) {
-            return res.status(400).json({message: "Username must be between 4 and 20 characters", error: true});
+            return res.status(400).json({message: "Username must be between 4 and 20 characters", error: true, success: false});
         }
         if (password.length < 4 || password.length > 20) {
-            return res.status(400).json({message: "Password must be between 4 and 20 characters", error: true});
+            return res.status(400).json({message: "Password must be between 4 and 20 characters", error: true, success: false});
         }
         next();
     },
@@ -26,7 +26,7 @@ module.exports = {
         password = password.trim();
 
         if (username.length < 4 || username.length > 20) {
-            return res.status(400).json({message: "Username must be between 4 and 20 characters", error: true});
+            return res.status(400).json({message: "Username must be between 4 and 20 characters", error: true, success: false});
         }
 
         if (password.length < 4 || password.length > 20) {
